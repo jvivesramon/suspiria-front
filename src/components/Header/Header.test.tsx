b66e439c6e7500ff885a390905fa26acdf947e3d
Judit Vives <jvivesramon@gmail.com>
2023-06-01 17:40:07 +0200

    Add Header component test for not isLogged case

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,17 +1,31 @@
 import { screen } from "@testing-library/react";
 import Header from "./Header";
 import { renderWithProviders } from "../../testUtils/testUtils";
+import {
+  initialUserStateMock,
+  userStateMock,
+} from "../../mocks/mocks/userMocks";
 
 describe("Given a Header component", () => {
   describe("When it is rendered", () => {
-    test("Then it should show a header with the Suspiria logo", () => {
+    test("Then it should show a header with the Suspiria logo when the user isn't logged", () => {
       const imageText = "Suspiria logo";
 
-      renderWithProviders(<Header />);
+      renderWithProviders(<Header />, { userStore: initialUserStateMock });
 
       const expectedResult = screen.getByRole("img", {
         name: imageText,
       });
+
+      expect(expectedResult).toBeInTheDocument();
+    });
+
+    test("Then it should show a header with the 'Home' icon nav when the user is logged", () => {
+      const imageText = "Home";
+
+      renderWithProviders(<Header />, { userStore: userStateMock });
+
+      const expectedResult = screen.getByText(imageText);
 
       expect(expectedResult).toBeInTheDocument();
     });
