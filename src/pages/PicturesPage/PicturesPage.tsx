3d7c85bed9c308eb691bd83c63837f6096efdb64
Judit Vives <jvivesramon@gmail.com>
2023-06-03 17:11:38 +0200

    Send pictures list to picturesSlice from PicturesPage page

--- src/pages/PicturesPage/PicturesPage.tsx
+++ src/pages/PicturesPage/PicturesPage.tsx
@@ -1,6 +1,14 @@
+import { picturesMock } from "../../mocks/mocks/picturesMock";
+import { useAppDispatch } from "../../store";
+import { loadPicturesActionCreator } from "../../store/pictures/picturesSlice";
 import PicturesPageStyled from "./PicturesPageStyled";
 
 const PicturesPage = (): React.ReactElement => {
+  const dispatch = useAppDispatch();
+  const { pictures } = picturesMock;
+
+  dispatch(loadPicturesActionCreator(pictures));
+
   return (
     <PicturesPageStyled>
       <img
