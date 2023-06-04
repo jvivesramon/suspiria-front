2a841a472b9ce5cbdeb16d5decd84d49c39c83ae
Judit Vives <jvivesramon@gmail.com>
2023-06-04 12:54:01 +0200

    Add useApi custom hook

--- src/hooks/useUser/useUser.ts
+++ src/hooks/useUser/useUser.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { UserCredentials } from "../../components/types";
 import { errorMessage } from "../../utils/errorMessages";
+import path from "../../routers/paths/paths";
 
 const useUser = () => {
   const apiUrl = import.meta.env.VITE_API_URL;
@@ -9,7 +10,10 @@ const useUser = () => {
     try {
       const {
         data: { token },
-      } = await axios.post<{ token: string }>(`${apiUrl}/user/login`, userData);
+      } = await axios.post<{ token: string }>(
+        `${apiUrl}${path.user}${path.login}`,
+        userData
+      );
 
       return token;
     } catch {
