116e9f1058ad1a6e811bb9449a63165e9e22085d
Judit Vives <jvivesramon@gmail.com>
2023-06-05 17:55:11 +0200

    Add showModal reducer

--- src/store/ui/uiSlice.ts
+++ src/store/ui/uiSlice.ts
@@ -1,22 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-interface UiSliceStructure {
+interface FeedbackStateStructure {
+  text?: string;
+  isError: boolean;
+  modalActionText?: string;
+}
+
+export interface UiSliceStructure {
   isLoading: boolean;
+  isVisible: boolean;
+  state: FeedbackStateStructure;
 }
 
 const uiState: UiSliceStructure = {
   isLoading: false,
+  isVisible: false,
+  state: {
+    isError: false,
+    modalActionText: "",
+    text: "",
+  },
 };
 
 const uiSlice = createSlice({
   name: "ui",
   initialState: uiState,
   reducers: {
-    showLoading: () => ({
+    showLoading: (currentState: UiSliceStructure) => ({
+      ...currentState,
       isLoading: true,
     }),
-    hideLoading: () => ({
+    hideLoading: (currentState: UiSliceStructure) => ({
+      ...currentState,
       isLoading: false,
+    }),
+    showModal: (
+      currentState: UiSliceStructure,
+      action: PayloadAction<FeedbackStateStructure>
+    ) => ({
+      ...currentState,
+      isVisible: true,
+      state: { ...action.payload },
     }),
   },
 });
@@ -24,6 +48,7 @@ const uiSlice = createSlice({
 export const {
   showLoading: showLoadingActionCreator,
   hideLoading: hideLoadingActionCreator,
+  showModal: showModalActionCreator,
 } = uiSlice.actions;
 
 export const uiReducer = uiSlice.reducer;
